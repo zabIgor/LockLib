a94b6889ae4553fd3decc3dec858a53701ebad9d
zabIgor <IgorZaby@yandex.com>
2025-01-09 17:39:32 +0300

    The language class has been added to the Book class in themodels.py

--- LockLib/catalog/models.py
+++ LockLib/catalog/models.py
@@ -48,7 +48,9 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book")
-
+    language = models.ForeignKey(
+        'Language', on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -56,7 +58,10 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
-    
+
+
+
+
 class BookInstance(models.Model):
 
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
