524ce6364ffaebeb7709f9823e956167e7a90d92
zabIgor <IgorZaby@yandex.com>
2025-02-19 17:11:11 +0300

    Add class BookAdmin(admin.ModelAdmin):

--- LockLib/catalog/admin.py
+++ LockLib/catalog/admin.py
@@ -2,9 +2,39 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
+#admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth') #'date_of_death')
+    #fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    #inlines = [BooksInline]
+    
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author') #'display_genre')
+
+
+
+
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
 
